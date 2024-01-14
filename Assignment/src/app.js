de9776b7068922050dea0ec6a925075ec1aa0891
yogeshprakhar <yogeshprakharpal200027@gmail.com>
2024-01-14 20:43:51 +0530

    update naming

--- Assignment/src/app.js
+++ Assignment/src/app.js
@@ -16,14 +16,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
-app.use(cookieParser());
 
 // ROUTES
 import patientRouter from "./routes/patient.route.js"
-// import { psychiatristDetail } from "./controllers/patient.controller.js"
 
 app.use("/patient",patientRouter)
 
-// app.use("/hospital",patientRouter)
 
 export {app}
